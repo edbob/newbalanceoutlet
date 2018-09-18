e7b91b77b28faf08fdc93288446f32d756a1a835
edbob <nedov@outlook.com>
2018-09-18 13:14:31 +0300

    settimeout

--- routes/contact.js
+++ routes/contact.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var nodemailer = require('nodemailer');
 var configur = require("../db/db.json")
-//var colors = require('colors');
+var colors = require('colors');
+var Promise = require('promise');
 //var hostName = process.env.host || "localhost:";
 
 var contact = express.Router();
@@ -11,42 +12,44 @@ contact.get('/contact', function (req, res) {
 });
 
 var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      type: 'OAuth2',
-      user: 'doess.not.smoke.here@gmail.com',
-      clientId: this.clientId = configur.clientId,
-      clientSecret: this.clientId = configur.clientSecret,
-      refreshToken: this.refreshToken = configur.refreshToken
-    }
-  });
-  
-  //var url = require('url');
-  var dateNow = new Date().toString().slice(8, 24);
+  service: 'gmail',
+  auth: {
+    type: 'OAuth2',
+    user: 'does.not.smoke.here@gmail.com',
+    clientId: configur.clientId,
+    clientSecret: configur.clientSecret,
+    refreshToken: configur.refreshToken
+  }
+});
 
-  contact.post('/contact', function (req, res) {
+//var url = require('url');
+var dateNow = new Date().toString().slice(8, 24);
 
-    var senderConf = {
-      Sendname: req.body.name,
-      Sendemail: req.body.email
-    }
+contact.post('/contact', function (req, res) {
 
-    var mailOptions = {
-      from: senderConf.Sendname + '<'+senderConf.Sendemail+'>',
-      to: 'nedov@outlook.com',
-      subject: req.body.name,
-      html: req.body.message + ' <br /> <p><b>Email отправителя: '+ req.body.email + '<br />отправлено с сайта: ' + req.headers.host + ' </b></p>'
+  var senderConf = {
+    Sendname: req.body.name,
+    Sendemail: req.body.email
+  }
+
+  var mailOptions = {
+    from: senderConf.Sendname + '<' + senderConf.Sendemail + '>',
+    to: 'nedov@outlook.com',
+    subject: req.body.name,
+    html: req.body.message + ' <br /> <p><b>Email отправителя: ' + req.body.email + '<br />отправлено с сайта: ' + req.headers.host + ' </b></p>'
+  };
+
+  transporter.sendMail(mailOptions, function (err, res) {
+    if (err) {
+      console.log('Error: '.red + err.message + ' [' + dateNow + ']');
+    } else {
+      console.log('Email Sent from: ' + req.body.email + "'".green + ' [' + dateNow + ']');
     };
-  
-    transporter.sendMail(mailOptions, function (err, res) {
-      if (err) {
-        console.log('Error: '.red + err.message + ' [' + dateNow + ']');
-      } else {
-        console.log('Email Sent from: ' + req.body.email + "'".green + ' [' + dateNow + ']');
-      }
-    });
-  
-    res.send('Thanks for contacting us, ' + req.body.name + '! We will respond shortly!');
   });
+  res.send('Thanks for contacting us, ' + req.body.name + '! We will respond shortly!');
+  // setTimeout(function () {
+  //   res.redirect('/contact');
+  // }, 3000);
+});
 
-  module.exports = contact;
+module.exports = contact;
